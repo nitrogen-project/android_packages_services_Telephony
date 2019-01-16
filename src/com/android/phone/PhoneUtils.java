7b28abbc2f61a5cdfab14ce0ded8f1ffe336b8ee
xyyx <xyyx@mail.ru>
2019-01-16 11:21:27 +0800

    USSD: Fix alert dialog theme
    We have light theme by default, but google uses dark theme
    example:
    https://github.com/nitrogen-project/android_frameworks_base/blob/27a4500227bd5fb42f6c67e5b1be6307ea2ae19b/core/java/android/app/AlertDialog.java#L215

--- src/com/android/phone/PhoneUtils.java
+++ src/com/android/phone/PhoneUtils.java
@@ -123,7 +123,7 @@ public class PhoneUtils {
      * because these dialogs are displayed using the application context, which does not resolve
      * the dialog theme correctly.
      */
-    private static final int THEME = com.android.internal.R.style.Theme_DeviceDefault_Dialog_Alert;
+    private static final int THEME = com.android.internal.R.style.Theme_DeviceDefault_Light_Dialog_Alert;
 
     private static class FgRingCalls {
         private Call fgCall;
